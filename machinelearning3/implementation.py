e4bd9a707f62bf8a546eeae736f5c348e310fe7d
Shankar G L <shankarshankargl2007@gmail.com>
2026-09-16 18:40:07 +0530

    update file

--- machinelearning3/implementation.py
+++ machinelearning3/implementation.py
@@ -1,3 +1,4 @@
+'''
 import numpy as np
 
 X = np.array([1, 2, 3, 4, 5, 6])
@@ -41,3 +42,4 @@ print("Weight:", w)
 print("Bias:", b)
 print("Probabilities:", probabilities)
 print("Predictions:", predictions)
+'''
